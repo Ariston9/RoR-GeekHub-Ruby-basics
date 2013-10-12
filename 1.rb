6515857c17465accba416ada145a2e80c808bb5b
shcherb <shcherb@ukr.net>
2013-10-13 00:30:53 +0300

    Create 1.rb

---
+++ 1.rb
@@ -0,0 +1,7 @@
+age = 1_000_000_000
+print "Person whose age is "+age.to_s+" seconds has lived #{ age / i = 60 * 60 * 24 } days, #{ age / i = i * 30 } monthes and #{ age = age/ i = i * 365 } year"
+if age > 1
+	print "s"
+end
+puts "."
+	
