cc568c6e1c21f4438474ef31392b75d4b6f8a2f9
shcherb <shcherb@ukr.net>
2013-10-13 17:55:37 +0300

    Update 1.rb
    Lesson 1. Task 1.

--- 1.rb
+++ 1.rb
@@ -1,7 +1,11 @@
-age = 1_000_000_000
-print "Person whose age is "+age.to_s+" seconds has lived #{ age / i = 60 * 60 * 24 } days, #{ age / i = i * 30 } monthes and #{ age = age/ i = i * 365 } year"
-if age > 1
-	print "s"
-end
-puts "."
+aage = 1_000_000_000
+sec_in_day = 60 * 60 *24   # секунд в дне
+sec_in_year = sec_in_day * 365.2425  # секунд в году. Длительность года в григорианском календаре 365.2425 дней
+year = age/sec_in_year.to_i  # к-во лет в 1 млн. сек.
+ost = age-year*sec_in_year  # сколько секунд осталось за вычетом 31 года
+ost_day = ost/sec_in_day  # узнаем сколько дней в остатке
+month = (ost_day/(365.2425/12)).to_i  # сколько месяцев в остатке
+day = (ost_day - month*365.24/12).to_i # сколько осталось дней за вычетом 8 месяцев
+puts "Person whose age is #{age} seconds has lived #{year} years, #{month} monthes and #{day} days."
 	
+
